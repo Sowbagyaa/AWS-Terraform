703c73ad00ff14f5ea94525feb8fea49e0e6f99e
Sowbagyaa Baskaran <sowrithis.30@gmail.com>
2025-07-04 02:56:27 +0530

    Update eks-cluster.tf

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,6 +1,6 @@
 module "eks" {
     source  = "terraform-aws-modules/eks/aws"
-    version = ">20.35.0"
+    version = "20.37.1"
     cluster_name = "poc-eks-cluster"
     cluster_version = "1.33.2"
 
