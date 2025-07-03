8935675097b448e6237837b7f20f660ebdf45ef4
Sowbagyaa Baskaran <sowrithis.30@gmail.com>
2025-07-04 01:52:05 +0530

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,7 +1,7 @@
 data "aws_availability_zones" "azs" {}
 module "myapp-vpc" {
   source          = "terraform-aws-modules/vpc/aws"
-  version         = ">6.0.0"
+  version         = ">5.20.0"
   name            = "myapp-vpc"
   cidr            = var.vpc_cidr_block
   private_subnets = var.private_subnet_cidr_blocks
